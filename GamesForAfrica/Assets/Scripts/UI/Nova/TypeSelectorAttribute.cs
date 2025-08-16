c6360db3de22d0cad9e962bbb4ad0ce1ba6787d4
Fate2664 <18pielou@gmail.com>
2025-08-16 09:44:25 +0200

    Moving to 2D

--- GamesForAfrica/Assets/Scripts/UI/Nova/TypeSelectorAttribute.cs
+++
@@ -1,9 +0,0 @@
-// Assets/Scripts/SettingsMenu/TypeSelectorAttribute.cs
-using System;
-using UnityEngine;
-
-namespace NovaSamples.SettingsMenu
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public class TypeSelectorAttribute : PropertyAttribute { }
-}
