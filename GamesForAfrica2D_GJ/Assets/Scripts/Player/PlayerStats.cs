92d50520b292d3a4af76b97859ea8440c0e002e5
Ember Willow Jones <st10450923@vgconnect.edu.za>
2025-08-17 10:00:08 +0200

    Refactored PlayerStats script

--- GamesForAfrica2D_GJ/Assets/Scripts/Player/PlayerStats.cs
+++ GamesForAfrica2D_GJ/Assets/Scripts/Player/PlayerStats.cs
@@ -3,24 +3,12 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     //Attack
-    public float attackDamage = 10f;
-    public float attackRange = 2f;
-    public float attackSpeed = 1f;
-    public float bullets = 3f;
-    public float bulletSpeed = 5f;
-    public float spreadAngle = 30f;
+    public float attackDamage;
+    public float attackRange;
+    public float attackSpeed;
+    public float bullets;
+    public float bulletSpeed;
+    public float spreadAngle ;
     //Movement
-    public float moveSpeed = 5f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float moveSpeed;
 }
